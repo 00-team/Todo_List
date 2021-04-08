2cf4d0378028f03b981e529d4413bb3d72e6c6bc
sadra <sadrataghavi1383@gmail.com>
2021-04-08 20:44:18 +0430

    adding final steps there is just a btn left over for deleting Todo object

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from to_do_app.models import Todo
 # Create your views here.
 
 
+@csrf_exempt
 def home(request):
-    return render(request,"base.html")
+    stuff_for_frontend = Todo.objects.all().order_by("-added_date")
+    return render(request,"base.html",{
+        "todo_items" : stuff_for_frontend
+        })
+
+@csrf_exempt
+def add_search(request):
+    now = timezone.now()
+    content = request.POST["input"]
+    if content:
+        created_obj = Todo.objects.create(added_date=now,text=content)
+    else:
+        created_obj = Todo.objects.create(added_date=now, text="Now Set")
+    print(created_obj)
+    stuff_for_frontend = Todo.objects.all().order_by("-added_date")
+    return render(request, "to_do_list/index.html",{
+        "todo_items": stuff_for_frontend
+    })
+
