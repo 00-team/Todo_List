c377729f03df200362a0a7ad435bfd63fe10e9a7
i007c <dr007cc@gmail.com>
2021-04-10 22:01:15 +0430

    make apis

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from to_do_app.models import Todo
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 
+import json
 
 @csrf_exempt
 def home(request):
@@ -27,15 +28,15 @@ def add_search(request):
     return HttpResponseRedirect("/")
 
 
-@csrf_exempt
-def delete(request,todo_id):
-    Todo.objects.get(id=todo_id).delete()
-    return HttpResponseRedirect("/")
+# @csrf_exempt
+# def delete(request,todo_id):
+#     Todo.objects.get(id=todo_id).delete()
+#     return HttpResponseRedirect("/")
 
 
-@csrf_exempt
-@require_POST
-def get_all_todos(r):
+# @require_POST
+# @csrf_exempt
+def get_all_todos(request):
     todos = Todo.objects.all().order_by("-added_date")
     ts = []
     for todo in todos:
@@ -46,4 +47,99 @@ def get_all_todos(r):
             "important": todo.important
         })
     
-    JsonResponse(ts)
+    return JsonResponse({"data":ts})
+
+
+@require_POST
+def delete_todo(r):
+    data = r.body
+
+    try:
+        data = json.loads(data)
+    except json.JSONDecodeError:
+        return JsonResponse({
+            "error":"data is not Valid"
+        })
+
+    if not data.get("tid"):
+        return JsonResponse({
+            "error":"data is not Valid"
+        })
+
+
+    tid = data.get("tid")
+
+    if type(tid) == str:
+        if tid.isnumeric():
+            tid = int(tid)
+
+    if not Todo.objects.filter(id=tid).exists():
+        return JsonResponse({
+            "error":"Todo Not Found"
+        })
+    
+    t = Todo.objects.get(id=tid)
+    t.delete()
+
+    return JsonResponse({
+        "sucsess":"Todo now Deleted"
+    })
+
+
+
+@require_POST
+def save_todo(r):
+    data = r.body
+
+    try:
+        data = json.loads(data)
+    except json.JSONDecodeError:
+        return JsonResponse({
+            "error":"data is not Valid"
+        })
+    
+    if not data.get("title") or not data.get("description"):
+        return JsonResponse({
+            "error":"data is not Valid"
+        })
+
+    if data.get("tid"):
+        tid = data.get("tid")
+
+        if type(tid) == str:
+            if tid.isnumeric():
+                tid = int(tid)
+
+        if not Todo.objects.filter(id=tid).exists():
+            return JsonResponse({
+                "error":"Todo Not Found"
+            })
+        
+        t = Todo.objects.get(id=tid)
+        t.title = data.get("title")
+        t.description = data.get("description")
+        t.important = bool(data.get("important")) or False
+        t.save()
+
+        return JsonResponse({
+            "sucsess":"Todo now Edited"
+        })
+
+    else:
+        t = Todo.objects.create(
+            title=data.get("title"),
+            description=data.get("description"),
+            important=bool(data.get("important")) or False,
+            added_date=timezone.now()
+        )
+        t.save()
+
+        return JsonResponse({
+            "sucsess":"Todo now Added",
+            "todo": {
+                "id":t.id,
+                "title":t.title,
+                "description":t.description,
+                "important":t.important,
+            }
+        })
