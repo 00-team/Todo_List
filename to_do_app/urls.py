2e02e1d0105751822e211c4bbe9b05ddf0649cc9
i007c <dr007cc@gmail.com>
2021-04-10 22:00:52 +0430

    make urls for todo api

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("add_search/", views.add_search, name="add_search"),
-    path("delete_todo/<int:todo_id>/", views.delete, name="delete"),
-
 
     path("gats/", views.get_all_todos, name="Get All Todos"),
+    path("dt/", views.delete_todo, name="Delete Todo"),
+    path("st/", views.save_todo, name="Save Todo"),
 ]
